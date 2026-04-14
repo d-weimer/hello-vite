f75ae9cd7485aa41b14187100aa46f5cde885dea
d-weimer <dweimer19@gmail.com>
2026-04-15 05:24:45 +0900

    Dynamic images - dog, cat, default

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import "../App.css";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
+import Animal from "./Animal.jsx";
 
 function App() {
   return (
     <div className="page">
       <Header />
       <Main />
+      <Animal selectedAnimal="dog" />
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const data = [
+  {
+    name: "default",
+    image: new URL("../assets/default.jpeg", import.meta.url).href,
+  },
+  {
+    name: "cat",
+    image: new URL("../assets/cat.jpeg", import.meta.url).href,
+  },
+  {
+    name: "dog",
+    image: new URL("../assets/dog.jpeg", import.meta.url).href,
+  },
+];
