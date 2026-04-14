8c763ae2d15bf34373810f3a7e65ef8671add87e
d-weimer <dweimer19@gmail.com>
2026-04-15 04:42:34 +0900

    Added TripleTen image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import headerLogo from "../assets/logo.png";
+
 function Header() {
   return (
     <div>
+      <img src={headerLogo} alt="App logo" />
       <h1>Hello Vite!</h1>
     </div>
   );
